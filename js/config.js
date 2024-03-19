bb3317f482b22c75a581995a36e5c42ccd15c02e
Zeke1996 <159496327+Zeke1996@users.noreply.github.com>
2024-03-19 18:37:24 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g.us-west-2_sZsvQZ6dR
-        userPoolClientId: '', // e.g. 553las8kphjci3596c8qrtpnb1
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_8yU4ACkCf', // e.g. us-west-2_8yU4ACkCf
+        userPoolClientId: '5e36btalpa8v9evaq2sbvrgjch', // e.g. 5e36btalpa8v9evaq2sbvrgjch
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
